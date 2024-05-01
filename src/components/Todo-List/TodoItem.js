7f0ac71904e71ff750be587ecd65a0b591920ed1
asad0411 <Muhammad.Asad@pixeledge.io>
2024-05-01 09:14:59 +0500

    tailwind added to project

--- src/components/Todo-List/TodoItem.js
+++ src/components/Todo-List/TodoItem.js
@@ -5,10 +5,12 @@ function TodoItem({ task, deleteTask, toggleCompleted }) {
   }
 
   return (
+    <div className="space-x-2 flex text-sm">
     <div className="todo-item">
       <input type="checkbox" checked={task.completed} onChange={handleChange} />
-      <p>{task.text}</p>
+      <p className="text-sky-500 dark:text-sky-400">{task.text}</p>
       <button onClick={() => deleteTask(task.id)}>X</button>
+    </div>
     </div>
   );
 }

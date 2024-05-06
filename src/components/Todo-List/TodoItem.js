dfe64b26f14fef56d9ca132255a69af34c14695d
asad0411 <Muhammad.Asad@pixeledge.io>
2024-05-06 14:42:00 +0500

    some more changes

--- src/components/Todo-List/TodoItem.js
+++ src/components/Todo-List/TodoItem.js
@@ -5,13 +5,13 @@ function TodoItem({ task, deleteTask, toggleCompleted }) {
   }
 
   return (
-    <div className="space-x-2 flex text-sm">
+    // <div className="space-x-2 flex text-sm">
     <div className="todo-item">
-      <input type="checkbox" checked={task.completed} onChange={handleChange} />
+      <input type="checkbox" id={task.id} checked={task.completed} onChange={handleChange} />
       <p className="text-sky-500 dark:text-sky-400">{task.text}</p>
       <button onClick={() => deleteTask(task.id)}>X</button>
     </div>
-    </div>
+    // </div>
   );
 }
 export default TodoItem;
